c2f4bd2fedd209635f2a53ae118ef2a46edd7f8a
xma1xma-maker <xma1xma@gmail.com>
2026-09-07 15:28:47 +0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 // استبدل هذه الروابط لاحقاً بروابط مشروعك في Supabase
-const supabaseUrl = 'YOUR_SUPABASE_URL';
-const supabaseKey = 'YOUR_SUPABASE_ANON_KEY';
+const supabaseUrl = 'https://duzreyfiwfqhzzwgivdd.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR1enJleWZpd2ZxaHp6d2dpdmRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg3ODAzMTEsImV4cCI6MjEwNDM1NjMxMX0.T1w4AmbXhRh3t24pa6tf9GfNRX8WMlk_5JpUFCzIZ4Y';
 
 export const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
